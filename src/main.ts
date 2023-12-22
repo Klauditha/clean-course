0909f4621c3c75f6f8ef326b10154430e53f68dc
Claudia Villarroel <claudia.villarroel@live.com>
2023-12-22 09:17:56 -0300

    :hammer:  Ejemplo SRP

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import './style.css'
 //import './clean-code/06-classes-c'
 //import './clean-code/07-tarea'
-import './code-smells/02-low-coupling'
+///import './code-smells/02-low-coupling'
+import './solid/01-srp'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
