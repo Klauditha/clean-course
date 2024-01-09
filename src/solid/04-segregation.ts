a92499937564fd5ef3803c15637b725ac4ffb640
Claudia Villarroel <claudia.villarroel@live.com>
2024-01-09 12:40:37 -0300

    :hammer: Aplicando segregacion de  interfaz

--- src/solid/04-segregation.ts
+++ src/solid/04-segregation.ts
@@ -1,40 +1,32 @@
-interface Bird{
-    fly(): void;
-    eat(): void;
-    run(): void;
-    swim(): void;
+interface Bird {
+  eat(): void;
 }
 
-class Tucan implements Bird {
+interface FlyingBird {
+  fly(): void;
+}
+interface RunningBird {
+  run(): void;
+}
+interface SwimmerBird {
+  swim(): void;
+}
+class Tucan implements Bird, FlyingBird {
   public fly() {}
   public eat() {}
-  public run() {}
-  public swim() {} //No aplica para esta ave
 }
 
-class Humminbird implements Bird {
+class Humminbird implements Bird, FlyingBird {
   public fly() {}
   public eat() {}
-  public run() {}
-  public swim() {} //No aplica para esta ave
 }
 
-class Ostrich implements Bird {
-  public fly() {
-    throw new Error("Ostrich can't fly");
-  }
+class Ostrich implements Bird, RunningBird {
   public eat() {}
   public run() {}
+}
+
+class Penguin implements Bird, SwimmerBird {
+  public eat() {}
   public swim() {}
 }
-
-
-class Penguin implements Bird {
-    public fly() {
-      throw new Error("Penguin can't fly");
-    }
-    public eat() {}
-    public run() {}
-    public swim() {}
-  }
-  
