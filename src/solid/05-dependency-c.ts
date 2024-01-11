c11efa66ed524c048675578dd05f50ceb2f1e9cf
Claudia Villarroel <claudia.villarroel@live.com>
2024-01-11 17:01:03 -0300

    :hammer: Aplicar Inversión de dependencias y Substitución de Liskov

--- src/solid/05-dependency-c.ts
+++ src/solid/05-dependency-c.ts
@@ -1,5 +1,11 @@
-export class LocalDataBaseService {
-  async getFakePostsDatabase() {
+import localPosts from '../data/local-database.json';
+import { Post } from './05-dependency-b';
+
+export abstract class PostProvider {
+  abstract getPosts(): Promise<Post[]>;
+}
+export class LocalDataBaseService implements PostProvider {
+  async getPosts() {
     return [
       {
         userId: 1,
@@ -15,5 +21,21 @@ export class LocalDataBaseService {
         body: 'est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis qui aperiam non debitis possimus qui neque nisi nulla',
       },
     ];
+  }
+}
+
+export class JsonDatabaseService implements PostProvider {
+  async getPosts() {
+    return localPosts;
+  }
+}
+
+export class WebApiPostService implements PostProvider {
+  async getPosts(): Promise<Post[]> {
+    const response = await fetch(
+      'https://jsonplaceholder.typicode.com/posts'
+    );
+    const posts = await response.json();
+    return posts;
   }
 }
